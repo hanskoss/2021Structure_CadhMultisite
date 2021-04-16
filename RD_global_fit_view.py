fixed non-essential bugs in execution of final calculation

RD_global_fit_view.py
@@ -18,13 +18,15 @@ import datamodelfunctions as mainfuncts
 #import hkwatch
 
 
-linux='/home/hanskoss/data/Cadherin/nmrCad/procandcoll/TSnewsort/2020Feb/'
 windows='C:\\Users\\Hans\\Desktop\\TRANSFER\\2020Feb\\'
 linuxss='/home/hanskoss/scripts/relaxproc/savstat'
+
 windowsss='C:\\Users\\Hans\\Desktop\\TRANSFER\\relaxproc\\savstat'
 linux='/home/hanskoss/scripts/github/2021Structure_CadhMultisite/exptl_data/'
+linux='/home/hanskoss/data/Cadherin/nmrCad/procandcoll/TSnewsort/2020Feb/'
+
 windows='C:\\Users\\Hans\\Desktop\\TRANSFER\\2020Feb\\'
-linuxss='/home/hanskoss/scripts/Cadh11_multis/savstat'
+##linuxss='/home/hanskoss/scripts/Cadh11_multis/savstat'
 windowsss='C:\\Users\\Hans\\Desktop\\TRANSFER\\relaxproc\\savstat'
 savstatdir=linuxss
 path2020=linux#linux#windows
@@ -115,6 +117,8 @@ namresults='multix13_NEW_pfr_STU_errb_'
 namresults='multix13_NEW_pfr_STU_err3f_'
 namresults='multix13_NEW_pfr_STU_errb3_'
 namresults='multix13_NEW_pfr_FINAL_stage2b'
+namresults='FINAL_stage2d'
+namresults='FINAL_stage3_'
 #namresults='multix13_NEW_pfr_STU_FINAL_'
 ###namresults='multix13_NEW_pfr_STU_err3g_'
 #namresults='multix13_NEW_pfr_S2_'
@@ -259,7 +263,7 @@ import hkwatch
 hkwatch.parameterplotter2(PropAxesColl,ParameterColl2,[2],cost=costcoll2,lenresults=3,resnam=reslalmall,conc=['2.475','9.9'],figno=106) #32
 #%%
 pbrat_collect=[]
-for mm in np.arange(80):
+for mm in np.arange(16):
     pb_lo=[];pb_hi=[];pc_lo=[];pc_hi=[];k12_lo=[];k12_hi=[];k13_lo=[];k13_hi=[];k23_lo=[];k23_hi=[];pb_rat=[];pc_rat=[];k12r=[];k13r=[];
     for n in np.arange(mm):
         pb1=flatten(ParameterColl2[n].getparandbnds(PropAxesColl,'p',inclfilt=[['sites','name','B'],['conc','value','2.475']]))[0]

RDmath.py
@@ -7,7 +7,9 @@ from hkimports2 import smp
 from hkimports2 import np
 from hkimports2 import scp
 from hkimports2 import scplin
-from hkimports2 import scipyexpm
+from scipy.linalg import expm
+
+#from hkimports2 import expm
 
 smp.init_printing(pretty_print=True,num_columns=150)
 def anyisnotsymbol(listx,eq0):
@@ -424,7 +426,7 @@ def cestfunction(omegarflist,deltaAB,deltaAC,k12,k13,k23,pb,pc,w1x,R1,R2,B0,exac
         
         if exactx == 1:
             Z=threetriangfunctionx(dwa,dwb,dwc,k12,k13,k23,pb,pc,w1,R1,R2)
-            at =scipyexpm(trad*Z)    
+            at =expm(trad*Z)    
             m0 = np.array([0.5,0,0,1-pb-pc,0,0,pb,0,0,pc])
             m1= np.array([0.5,0,0,-(1-pb-pc),0,0,-pb,0,0,-pc])
             magA=at[3,3]*m0[3]+at[3,6]*m0[6]+at[3,9]*m0[9]

globalfitfunctions.py
@@ -434,6 +434,151 @@ def reshuffle(ss,reslalmall,shuffletype):
                 q+=1
     return ss
 
+def runfit4b(praxs1,ctd,selresidues,precalc,resnam,conditions,path2020,savstatdir,files,filenamsav,paramsx,drawonly,cond):
+    """ This function prepares the global fit
+    input arguments
+    praxs1: property axis collections
+    ctd
+    precalc: triggers different preparatory routines depending on this switch
+        0: regular fit, no pre-calculated data
+        1: uses re-sampled data supplied by cond[1].
+        2: loads a previously performed calculation and calls a dedicated \
+            re-sampling function.
+    
+    
+    """
+    global spinsystems
+
+    moreconditions=[ctd,selresidues,precalc,resnam,files]
+    if precalc != 0 and precalc != 1:
+        spinsystems=hkio.loadss(savstatdir,precalc)
+        reslalmall=selresidues[0]#[reslall[i] for i in pickthese]
+        shuffletype=[['cpmg','dataset'],['Rex','dataset'],['cest','each']]
+        spinsystems=reshuffle(spinsystems,reslalmall,shuffletype)
+    elif precalc == 1:
+        
+        reslalmall=selresidues
+   #     print reslalmall, 'reslalmall'
+        spinsystems,setlabels=prepro.launch(path2020,files)
+        nr=0
+        for ssn,spins in enumerate(spinsystems):
+            if spins.name[0] in reslalmall:
+       #         print ''
+        #        print spins.name[0]
+                #dsn=0
+                for dsn,ds in enumerate(spins.datasets):
+                    if ds.datatype in ['Rex', 'cpmg', 'cest']:
+                        if ds.datatype == 'Rex':
+                            spinsystems[ssn].datasets[dsn].reshufy=cond[1][nr][0]
+                        else:
+                            spinsystems[ssn].datasets[dsn].reshufy=cond[1][nr]
+          #              print 'reshuf'
+          #              print spinsystems[ssn].datasets[dsn].reshufy
+
+                        nr+=1
+                    #    dsn+=1
+        
+    elif precalc == 0:
+ #       print 'path'
+  #      print path2020,files,'!!!!!!'
+        spinsystems,setlabels=prepro.launch(path2020,files)
+    #    print vars(spinsystems[55].datasets[5])
+            #print i.xlabel
+  #  print 'p2'
+    if len(ctd) == 0:
+        resultcolll=[]
+        poscolll=[]
+        resnaml=[]
+        allresultcoll=[]
+    else:
+        resultcolll=ctd[0]
+        poscolll=ctd[1]
+        resnaml=resnam
+        allresultcoll=[]
+    for selectdatn in selresidues:
+      #  global expcnd
+        resnaml,timedat,rawdata,errd,field,field2,field3,tr,equationtype,poscoll,expcnd=prepro.passdatatofitn(spinsystems,selectdatn,precalc)
+   #     print rawdata, 'rawdata'
+   #ss[     print selectdatn, precalc, rawdata, 'step'
+        filters=[[['residues','name'],[[rn] for rn in resnaml]],[['conc','value'],[['2.475'],['9.9']]],[['TR','name'],[['T'],['X']]],[['B1field','rounded'],[[50],[70],[80],[90]]],[['type','name'],[['cpmg'],['Rex'],['cest']]]]#[['w1type','numb'],[[3],[6],[15],[25],[50]]]]
+        filters2=[[['conc','value'],[['2.475'],['9.9']]],[['TR','name'],[['T'],['X']]],[['B1field','rounded'],[[50],[70],[80],[90]]],[['type','name'],[['cpmg'],['Rex'],['cest']]]]#[['w1type','numb'],[[3],[6],[15],[25],[50]]]]
+        selset=[]
+        q=0
+        explist=[]
+        for j,i in enumerate(expcnd):
+            for l,k in enumerate(i):
+                selsetx=[j]
+              #  print j,i,l,k
+                for n,m in enumerate(filters2):
+                    selsetx.append([p for p,o in enumerate(m[1]) if k[m[0][0]] in o][0])
+                selset.append(selsetx)
+                explist.append(q)
+                q+=1
+        filt=[]
+        aa=selset
+       # print aa
+       # print len(aa), 'aa'
+        bb=set(tuple(ix) for ix in selset)
+        bb=[list(b) for b in aa]
+        seldatasets=list(np.arange(len(aa)))
+       # print seldatasets, 'seldatasets', aa
+        inclfx=[]
+        for l,i in enumerate(bb):
+            inclf=[]
+            for k,j in enumerate(i):
+                inclf.append([filters[k][0][0],filters[k][0][1],filters[k][1][j]])
+                    #inclf.append([filters[k-1][0][0],filters[k-1][0][1],filters[k-1][1][j]])
+                inclfx.append(inclf)
+            a,b1,b2,c,e=paramsx.getallparandbnds(praxs1,['p','k','dw','R20500','R2mult'],inclfilt=inclf)
+            f=flatten([np.array(e[m][:-1])+np.sum([k for k in [0]+[e[j][-1] for j,i in enumerate(e) if j < len(e)-1]][0:(l+1)]) for m,l in enumerate(np.arange(len(e)))])
+            filt.append(f)
+        timedat=[flatten(timedat,levels=1)[i] for i in seldatasets]
+        rawdata=[flatten(rawdata,levels=1)[i] for i in seldatasets]
+        errd=[flatten(errd,levels=1)[i] for i in seldatasets]
+        field=[flatten(field,levels=1)[i] for i in seldatasets]
+        field2=[flatten(field2,levels=1)[i] for i in seldatasets]
+        field3=[flatten(field3,levels=1)[i] for i in seldatasets]
+        tr=[flatten(tr,levels=1)[i] for i in seldatasets]
+        
+        equationtype=[flatten(equationtype,levels=1)[i] for i in seldatasets]
+        #paramsx.getallparandbnds()
+        a,b1,b2,c,e=paramsx.getallparandbnds(praxs1,['p','k','dw','R20500','R2mult'],inclfilt=[])
+        f=flatten([np.array(e[m][:-1])+np.sum([k for k in [0]+[e[j][-1] for j,i in enumerate(e) if j < len(e)-1]][0:(l+1)]) for m,l in enumerate(np.arange(len(e)))])
+        poscolll.append(poscoll)
+        if drawonly == 0:
+            sojetzt,allrescoll=fitcpmg4(path2020,savstatdir,praxs1,timedat,rawdata,field,errd,precalc,equationtype,conditions,filenamsav,resnaml, poscolll,moreconditions,paramsx,filt)
+        else:
+            dwbsetp=[]
+            setdwb=0
+            for i in np.arange(len(timedat)):
+                for j in np.arange(len(flatten(timedat[i]))):
+                    dwbsetp.append(setdwb)
+                setdwb+=1
+            par7=np.array(np.array(dwbsetp).astype('int'))
+       #     print timedat, 'timedat'
+        #    print rawdata, 'rawdata'
+        #    print field, 'field'
+         #   print errd, 'errd'
+            fittedcurve,chsq0,chsq1,chsq2,chsq3=printrd(praxs1,timedat,rawdata,field,errd,equationtype,par7,paramsx,filt)
+            print "overall chi quare", chsq2
+            for j,i in enumerate(seldatasets):
+                print i, 'datasetno', chsq0[j], equationtype[j][0]
+       # print chsq0, chsq1, chsq2, chsq3
+        if drawonly == 0:
+            try:
+                resultcolll.append(sojetzt)
+                allresultcoll.append(allrescoll)
+            except:
+                print 'ugh1'
+        else:
+            for j in poscoll:
+                for k,i in enumerate(seldatasets): #fittedcurve:
+                    spinsystems[j].datasets[i].fit=fittedcurve[k]
+    if drawonly == 0:
+        return resultcolll, resnaml, poscolll, spinsystems, allresultcoll
+    else:
+        return spinsystems#else:
+
 def runfit4(praxs1,ctd,selresidues,precalc,resnam,conditions,path2020,savstatdir,files,filenamsav,paramsx,drawonly,cond):
     """ This function prepares the global fit and converts data structures where
     appropriate.
@@ -494,8 +639,8 @@ def runfit4(praxs1,ctd,selresidues,precalc,resnam,conditions,path2020,savstatdir
                 explist.append(q)
                 q+=1
         filt=[]
+        
         aa=selset
-       # print aa
        # print len(aa), 'aa'
         bb=set(tuple(ix) for ix in selset)
         bb=[list(b) for b in aa]
@@ -540,7 +685,6 @@ def runfit4(praxs1,ctd,selresidues,precalc,resnam,conditions,path2020,savstatdir
             par7=np.array(np.array(dwbsetp).astype('int'))
             fittedcurve,chsq0,chsq1,chsq2,chsq3=printrd(praxs1,timedat,\
                             rawdata,field,errd,equationtype,par7,paramsx,filt)
-            print "overall chi quare", chsq2
             for j,i in enumerate(seldatasets):
                 print i, 'datasetno', chsq0[j], equationtype[j][0]
         if drawonly == 0:
@@ -586,10 +730,21 @@ def evaluaterdfit(path2020,savstatdir,praxs1,setparameters2,runnum,paramsx,preca
     temp1,temp2,temp3,temp4,temp5,temp6,temp7,temp8,temp9,temp10,temp11\
     ,temp12,temp13,temp14,temp15,temp16,temp17,temp18,cond=hkio.\
     loadeverything(savstatdir,[filenamsav],0,decoupl=0)
+    print 'pointa'
     ss=runfit4(praxs1,[],selresidues,precalc2,resnam,conditions,path2020,savstatdir,\
     databasis,filenamsav+str(runnum),paramsx,1,cond)
     return ss
     #return spinsystems,fittedcurve
 
-   
+def evaluaterdfitold(path2020,savstatdir,praxs1,setparameters2,runnum,paramsx,precalc2):
+    time.sleep(runnum/10)
+    databasis,datapath,selresidues,conditions,filenamsav=setparameters2
+    #selresidues=[]
+    os.chdir(datapath)
+    resultcoll=[];poscoll=[];resnam=[]
+    temp1,temp2,temp3,temp4,temp5,temp6,temp7,temp8,temp9,temp10,temp11,temp12,temp13,temp14,temp15,temp16,temp17,temp18,cond=hkio.loadeverything(savstatdir,[filenamsav],0,decoupl=0)
+   # print cond
+    ss=runfit4(praxs1,[],selresidues,precalc2,resnam,conditions,path2020,savstatdir,databasis,filenamsav+str(runnum),paramsx,1,cond)
+    
+    return ss   
    

goglobalfit.py
@@ -11,14 +11,14 @@ import iofunctions as hkio2
 import datamodelfunctions as mainfuncts
 from hkimports2 import flatten
 from hkimports2 import np
-from hkimports2 import copy
+import copy
 from hkimports2 import multiprocessing
 
 linux='/home/hanskoss/data/Cadherin/nmrCad/procandcoll/TSnewsort/2020Feb/'
-linux='/home/hanskoss/scripts/github/2021Structure_CadhMultisite/exptl_data/'
+linux='/home/hanskoss/scripts/Github/2021Structure_CadhMultisite/exptl_data/'
 windows='C:\\Users\\Hans\\Desktop\\TRANSFER\\2020Feb\\'
 linuxss='/home/hanskoss/scripts/relaxproc/savstat'
-linuxss='/home/hanskoss/scripts/Cadh11_multis/savstat'
+#linuxss='/home/hanskoss/scripts/Cadh11_multis/savstat'
 windowsss='C:\\Users\\Hans\\Desktop\\TRANSFER\\relaxproc\\savstat'
 savstatdir=linuxss
 path2020=linux#linux#windows
@@ -252,7 +252,7 @@ setparameters3=['combo10l.dat',path2020,[[x for y,x in enumerate(reslall) if y i
 
 mainfuncts.parallelmultifit4(path2020,savstatdir,setparameters3,1,20,ParameterColl2,PropAxesColl)
 
-
+#%%
 
 """Stage 3: extended fitting procedure / minimization to account for all 16 
 different possible combinations of ambiguous dwb and dwc"""
@@ -262,7 +262,7 @@ namresultsprev='FINAL_stage2d'
 
 """ read in results from stage 2d"""
 conditions=[0,[1,0,0,10,5]]
-PropAxesColl,ParameterColl2, costcoll, resultcoll,cond=readoutresults(reslall,resnall,pickthese,DeltaOmegaParametersBoundaries,namresultsprev,conditions)
+PropAxesColl,ParameterColl2, costcoll, resultcoll,cond=readoutresults(reslall,resnall,pickthese,DeltaOmegaParametersBoundaries,savstatdir,namresultsprev,conditions)
 reslalmall=[reslall[i] for i in pickthese]
 
 """ Filter out all results which match the final conditions for dwb and dwc
@@ -337,21 +337,21 @@ smaller than the arising from the subsequent error calculation"""
     
 conditions=[0,[1,0,0,5,5]]
 setparameters3=['combo10l.dat',path2020,[[x for y,x in enumerate(reslall) if y in pickthese]],conditions,'FINAL_stage3_',0]
-mainfuncts.parallelmultifit4(setparameters3,1,16,ParameterColl6,PropAxesColl)
+mainfuncts.parallelmultifit4(path2020,savstatdir,setparameters3,1,16,ParameterColl6,PropAxesColl)
 
-
-
+#%%
+import globalfitfunctions as hkfit2
 """ Stage 4 - error calculation. In this final stage, errors are obtained by
 resampling. For each of the 16 outputs from the previous calculation, 5
 parameter sets from randomly resampled data are calculated. This gives a
 total of 80 paarametersets to then report the error by calculating the standard
 deviation for each parameter """
 dataname=cond[-1][-1]
-setparameters2=[dataname,path2020,[[x for y,x in enumerate(reslall) if y in pickthese]],conditions,'FINAL_stage3_'] #,'A34' #'/home/hanskoss/data/Cadherin/nmrCad/procandcoll/TSnewsort/2020Feb/
+setparameters2=[dataname,path2020,[x for y,x in enumerate(reslall) if y in pickthese],conditions,'FINAL_stage3_'] #,'A34' #'/home/hanskoss/data/Cadherin/nmrCad/procandcoll/TSnewsort/2020Feb/
 for selset in np.arange(16):
-    ss=hkfit2.evaluaterdfit(PropAxesColl,setparameters2,1,ParameterColl6[selset],0)
-    hkio2.savss(ss,'FINAL_stage3x_'+str(selset))
-
+    ss=hkfit2.evaluaterdfit(path2020,savstatdir,PropAxesColl,setparameters2,1,ParameterColl6[selset],0)
+    hkio2.savss(savstatdir,ss,'FINAL_stage3x_'+str(selset))
+#%%
 conditions=[0,[1,0,0,5,5]]
 processes=[]
 """ The parallelization for this process is explicitly coded because each of 
@@ -365,7 +365,7 @@ for outnum in np.arange(outnum):
     for runnum in np.arange(numrep):
         print 'run ', str(outnum*numrep+runnum)
         setparameters3=['combo10l.dat',path2020,[[x for y,x in enumerate(reslall) if y in pickthese]],conditions,'FINAL_stage4_','FINAL_stage3x_'+str(runnum)]
-        p=multiprocessing.Process(target=hkfit2.parallelfit3, args=(setparameters3,outnum*numrep+runnum,ParameterColl6[runnum],PropAxesColl))
+        p=multiprocessing.Process(target=hkfit2.parallelfit3, args=(path2020,savstatdir,setparameters3,outnum*numrep+runnum,ParameterColl6[runnum],PropAxesColl))
         processes.append(p)
         processes[pn].start()
         pn+=1

hkimports2.py
@@ -28,6 +28,6 @@ import copy
 from datetime import datetime, date
 #import hk_cpmg_symbolic1_3
 import matplotlib.gridspec as gridspec 
-from scipy.linalg import expm as scipyexpm
+from scipy.linalg import expm
 from scipy.optimize import curve_fit
 

preprocessing.py
@@ -406,6 +406,7 @@ def adddata(spinsystems,spinlistpath,filetype,dataposition,setselect,setlabel,pa
     """
  #   print spinlistpath,filetype, setlabel, 'setlabel!'
     os.chdir(spinlistpath)
+##    print spinlistpath
     relpath='./'
     expcond={}
     try:
@@ -597,9 +598,9 @@ def adddata(spinsystems,spinlistpath,filetype,dataposition,setselect,setlabel,pa
         vdf=np.array([i[1] if j+1 not in trpos else 0 for j,i in enumerate(zip(vc,vd))])
         vcfs=np.sort(np.array(list(set(vcf))))
         vdfs=np.array([vdf[j] for j in [list(vcf).index(i) for i in vcfs]])
-        fdata=1/(4*vdfs)
-        decaytimes=vcfs*(p4pulsenumber*p2/1000000+p4pulsenumber*2*vdfs)
-        fdata=1/(4*(decaytimes/(vcfs*2*p4pulsenumber)))
+        vcfsno0=np.array([j if j > 0 else 0.00001 for j in vcfs])
+        decaytimes=vcfsno0*(p4pulsenumber*p2/1000000+p4pulsenumber*2*vdfs)
+        fdata=(vcfs*2*p4pulsenumber)/(4*(decaytimes))
         label=[];intens=[];peakno=[]
         
         with open('CPMGpeaks.dat','rb') as csvfile:
@@ -658,14 +659,19 @@ def adddata(spinsystems,spinlistpath,filetype,dataposition,setselect,setlabel,pa
                 j.append((dataint/trint)*np.sqrt((np.array(err/np.array(dataint))/np.array(flatten(np.sqrt(np.array(pntcnt)))))**2+(np.array((err/np.array(trint)))/np.sqrt(pntcntr))**2))
                 #print (dataint/trint)*np.sqrt((np.array(err/np.array(dataint))/np.array(flatten(np.sqrt(np.array(pntcnt)))))**2+(np.array((err/np.array(trint)))/np.sqrt(pntcntr))**2)
                 decaytimex=decaytimes[1:]
+                #print j[0]
                 ydata=([-np.log(j[0][i])*(1/decaytimex[i]) for i in np.arange(len(j[0]))])
                 if np.min([(j[0][i]-j[1][i]) for i in np.arange(len(j[0]))]) < 0:
+#                    print 'path1'
                     ydataerr=zip([-np.log(j[0][i])*(1/decaytimex[i])+np.log(j[0][i]+j[1][i])*(1/decaytimex[i]) for i in np.arange(len(j[0]))],[-np.log(j[0][i])*(1/decaytimex[i])+np.log(j[0][i]+j[1][i])*(1/decaytimex[i]) for i in np.arange(len(j[0]))])
                 else:
+#                    print 'path2'#, j[0], j[1], j[0]-j[1], j[0]+j[1]
                     ydataerr=zip([-np.log(j[0][i]-j[1][i])*(1/decaytimex[i])+np.log(j[0][i])*(1/decaytimex[i]) for i in np.arange(len(j[0]))],[-np.log(j[0][i])*(1/decaytimex[i])+np.log(j[0][i]+j[1][i])*(1/decaytimex[i]) for i in np.arange(len(j[0]))])
                 if fdata[-1] > 2000:
+           #         print fdata[1:-1]
                     spinsystems[nf].datasets[-1].addcpmgdata(setlabel,field,nitrotag,p2,fdata[1:-1],ydata[:-1],ydataerr[:-1],vcfs[1:-1],vdfs[1:-1],decaytimex) #[1,;]
                 else:
+          #          print fdata[1:]
                     spinsystems[nf].datasets[-1].addcpmgdata(setlabel,field,nitrotag,p2,fdata[1:],ydata,ydataerr,vcfs[1:],vdfs[1:],decaytimex) #[1,;]
              #   datasets[-1].addss(nf,l)
                 #print np.sqrt((err/dataint)**2+(err/trint)**2)
@@ -1101,6 +1107,7 @@ def launch(pathprefix,collection):
     cnt=0
     for sspos1, setselecta in enumerate(setselectlist[superselector]):
         for sspos2, setselect in enumerate(setselecta):
+##            print sspos1,sspos2, 'positionc'
      #       print pathprefix+pathlist[setselect],filetypelist[setselect],cnt,setselect,setlabels[setselect],pathlist, 'someparameters'
             spinsystems=adddata(spinsystems1,pathprefix+pathlist[setselect],filetypelist[setselect],cnt,setselect,setlabels[setselect],pathlist)
             cnt+=1